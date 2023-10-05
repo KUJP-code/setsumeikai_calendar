0aa92a535190fee35b31e8090171ea5d50d0bdbc
Brett Tanner <bretttanner171@gmail.com>
2023-10-06 00:28:33 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      base: "/wp-content/reactpress/apps/setsumeikai_calendar/dist/",
+      plugins: [react()],
+    }
+  } else {
+    return {
+      plugins: [react()],
+    }
+  }
+})
