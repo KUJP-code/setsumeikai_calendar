aab423bcd7ed780848c593e4b195291a909d89b9
Brett Tanner <bretttanner171@gmail.com>
2023-10-19 18:03:21 +0900

    Handle minutes less than 10

--- src/helpers/jpFormat.ts
+++ src/helpers/jpFormat.ts
@@ -1,3 +1,6 @@
 export default function jpFormat(date: Date) {
-  return `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+  return `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
 }
