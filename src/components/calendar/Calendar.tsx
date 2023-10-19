ae6a79f7a394ced17cc6e3eefb94a8f41a496e74
Brett Tanner <bretttanner171@gmail.com>
2023-10-19 15:26:50 +0900

    Change calendar view based on viewport width

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,16 +1,24 @@
 import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
 import FullCalendar from "@fullcalendar/react";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import { useLoaderData } from "react-router-dom";
 
 function responsiveView() {
   return window.innerWidth < 700 ? "listMonth" : "dayGridMonth";
 }
 
 export default function Calendar() {
+  const events: setsumeikai[] = useLoaderData() as setsumeikai[];
+
   return (
     <FullCalendar
       contentHeight={"auto"}
+      firstDay={1}
+      events={events}
+      eventBackgroundColor="#ef8200"
       initialView={responsiveView()}
+      locale={jaLocale}
       plugins={[dayGridPlugin, listPlugin]}
       viewClassNames={["p-3"]}
       windowResize={(viewAPI) =>
