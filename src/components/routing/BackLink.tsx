25578d4f63833df60a42b1deaeab46a21d9cbdeb
Brett <bretttanner171@gmail.com>
2023-10-20 11:49:51 +0900

    Split Back/ForwardLink out from FooterNav

---
+++ src/components/routing/BackLink.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+interface BackLinkProps {
+  currentStep: string;
+  schoolId: number;
+}
+
+export default function BackLink({ currentStep, schoolId }: BackLinkProps) {
+  return currentStep.includes("/school_list") ? null : currentStep.includes(
+      "/calendar"
+    ) ? (
+    <NavLink
+      to="/school_list"
+      className="bg-ku-secondary flex justify-center items-center text-neutral-100 font-semibold p-3  basis-1/2 md:basis-1/3 rounded"
+    >
+      Change School
+    </NavLink>
+  ) : (
+    <NavLink
+      to={`/calendar/${schoolId}`}
+      className="bg-ku-secondary flex justify-center items-center text-neutral-100 font-semibold p-3  basis-1/2 md:basis-1/3 rounded"
+    >
+      Change Setsumeikai
+    </NavLink>
+  );
+}
--- src/components/routing/FooterNav.tsx
+++ src/components/routing/FooterNav.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import jpFormat from "../../helpers/jpFormat";
+import BackLink from "./BackLink";
+import ForwardLink from "./ForwardLink";
 
 interface props {
   currentStep: string;
@@ -17,63 +16,17 @@ export default function FooterNav({
   setsumeikaiDate,
   setsumeikaiId,
 }: props) {
-  const className =
-    " flex justify-center items-center text-neutral-100 font-semibold p-3  basis-1/2 md:basis-1/3 rounded";
-
-  function handleClick(e: React.MouseEvent<HTMLAnchorElement>) {
-    if (currentStep.includes("/school_list") && schoolName === "") {
-      e.preventDefault();
-    }
-    if (currentStep === "/calendar" && setsumeikaiDate === undefined) {
-      e.preventDefault();
-    }
-  }
-
   return (
     <nav className="z-50 sticky bottom-0 flex justify-between gap-3 p-3 bg-white border rounded">
-      {/* Back link */}
-      {currentStep.includes("/school_list") ? null : currentStep.includes(
-          "/calendar"
-        ) ? (
-        <NavLink
-          to="/school_list"
-          className={"bg-ku-secondary".concat(...className)}
-        >
-          Change School
-        </NavLink>
-      ) : (
-        <NavLink
-          to={`/calendar/${schoolId}`}
-          className={"bg-ku-secondary".concat(...className)}
-        >
-          Change Setsumeikai
-        </NavLink>
-      )}
-
+      <BackLink currentStep={currentStep} schoolId={schoolId} />
       <div className="flex-grow"></div>
-
-      {/* Next link */}
-      {currentStep.includes("/school_list") ? (
-        <NavLink
-          to={`/calendar/${schoolId}`}
-          className={"bg-ku-orange".concat(...className)}
-          onClick={(e) => handleClick(e)}
-        >
-          {schoolName
-            ? `View calendar for ${schoolName}`
-            : "Select school to continue"}
-        </NavLink>
-      ) : currentStep.includes("/calendar") ? (
-        <NavLink
-          to={`/form/${schoolId}/${setsumeikaiId}`}
-          className={"bg-ku-orange".concat(...className)}
-          onClick={(e) => handleClick(e)}
-        >
-          {setsumeikaiDate
-            ? `Register for setsumeikai on ${jpFormat(setsumeikaiDate)}`
-            : "Select setsumeikai to continue"}
-        </NavLink>
-      ) : null}
+      <ForwardLink
+        currentStep={currentStep}
+        schoolId={schoolId}
+        schoolName={schoolName}
+        setsumeikaiDate={setsumeikaiDate}
+        setsumeikaiId={setsumeikaiId}
+      />
     </nav>
   );
 }
