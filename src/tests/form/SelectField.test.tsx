ba9f4953eb57e307151c6c84ef5c8eca761a2116
Brett Tanner <bretttanner171@gmail.com>
2023-10-20 16:58:22 +0900

    Test SelectField

---
+++ src/tests/form/SelectField.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, it } from "bun:test";
+import SelectField from "../../components/form/SelectField";
+
+describe("SelectField", () => {
+  beforeEach(() => {
+    render(
+      <SelectField
+        label="testLabel"
+        name="testName"
+        options={[
+          { name: "test1", value: "1" },
+          { name: "test2", value: "2" },
+        ]}
+        required={true}
+      />
+    );
+  });
+
+  it("renders a select input", () => {
+    expect(screen.findByRole("combobox")).toBeTruthy();
+  });
+
+  it("renders an option for each passed option", () => {
+    expect(screen.getAllByRole("option").length).toBe(2);
+  });
+});
