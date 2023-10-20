81817b47afeceb3ddffea2567f39cd098c9b8554
Brett Tanner <bretttanner171@gmail.com>
2023-10-20 16:42:16 +0900

    Test InputField

---
+++ src/tests/form/InputField.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "bun:test";
+import InputField from "../../components/form/InputField";
+
+describe("InputField", () => {
+  function renderInputField(type: inputType) {
+    render(
+      <InputField
+        type={type}
+        label="testLabel"
+        name="testName"
+        placeholder="testPlaceholder"
+        required={true}
+      />
+    );
+  }
+
+  it("renders input of passed type unless passed type == textarea", () => {
+    renderInputField("email");
+
+    expect(screen.getByRole("textbox")).toHaveProperty("type", "email");
+  });
+
+  it("renders textarea if it's the passed type", () => {
+    renderInputField("textarea");
+
+    expect(screen.getByRole("textbox")).toHaveProperty("type", "textarea");
+  });
+
+  it("input renders with all passed values", () => {
+    renderInputField("tel");
+    const input = screen.getByRole("textbox");
+
+    expect(input).toHaveProperty("type", "tel");
+    expect(input).toHaveProperty("name", "testName");
+    expect(input).toHaveProperty("placeholder", "testPlaceholder");
+    expect(input).toHaveProperty("required");
+  });
+});
