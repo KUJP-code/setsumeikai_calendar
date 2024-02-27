adeee5b7c0c3ed7141fd78b8ad4e4ce93f91d8f9
Brett <bretttanner171@gmail.com>
2024-02-27 16:51:58 +0900

    Add online exception

--- src/components/schools/SchoolCard.tsx
+++ src/components/schools/SchoolCard.tsx
@@ -7,35 +7,63 @@ interface props {
 }
 
 export default function SchoolCard({ school, setSchool }: props) {
-  return (
-    <NavLink
-      to={`/calendar/${school.id}/undefined`}
-      type="button"
-      className="flex flex-col justify-start basis-full md:basis-[21vw] gap-2 text-start bg-white p-2 rounded border-secondary text-sm text-ku-secondary"
-      onClick={() => setSchool(school.name)}
-    >
-      <div
-        className="h-[20vh] bg-cover bg-center"
-        style={{ backgroundImage: `url(${school.image})` }}
-      ></div>
-      <div className="flex flex-col justify-between gap-1">
-        <h5 className="font-bold text-2xl text-ku-orange text-center w-full">
-          {school.name}
-        </h5>
-        <p>{school.address}</p>
-        {school.busAreas.length ? (
-          <p>
-            <span className="font-semibold">送迎対象地域:</span>{" "}
-            {school.busAreas.join("、 ")}
-          </p>
-        ) : null}
-        {school.nearbyStations.length ? (
-          <p>
-            <span className="font-semibold">最寄駅:</span>{" "}
-            {school.nearbyStations.join("、 ")}
-          </p>
-        ) : null}
-      </div>
-    </NavLink>
-  );
+  if (school.name === "Kids UP オンラインコース") {
+    return (
+      <NavLink
+        to={`/calendar/${school.id}/undefined`}
+        type="button"
+        className="flex flex-col justify-start basis-full md:basis-[21vw] gap-2 text-start bg-white p-2 rounded border-secondary text-sm text-ku-secondary"
+        onClick={() => setSchool(school.name)}
+      >
+        <div
+          className="h-[20vh] bg-cover bg-center"
+          style={{ backgroundImage: `url(${school.image})` }}
+        ></div>
+        <div className="flex flex-col justify-between gap-1">
+          <h5 className="font-bold text-2xl text-ku-orange text-center w-full">
+            Kids UP オンラインコース
+          </h5>
+          楽しみながら学べるおうちで英会話！
+          <br />
+          KidsUPのレッスンをご家庭で！
+          <br />
+          お子様の学びをサポートするコンテンツを
+          <br />
+          豊富にご用意しております。
+        </div>
+      </NavLink>
+    );
+  } else {
+    return (
+      <NavLink
+        to={`/calendar/${school.id}/undefined`}
+        type="button"
+        className="flex flex-col justify-start basis-full md:basis-[21vw] gap-2 text-start bg-white p-2 rounded border-secondary text-sm text-ku-secondary"
+        onClick={() => setSchool(school.name)}
+      >
+        <div
+          className="h-[20vh] bg-cover bg-center"
+          style={{ backgroundImage: `url(${school.image})` }}
+        ></div>
+        <div className="flex flex-col justify-between gap-1">
+          <h5 className="font-bold text-2xl text-ku-orange text-center w-full">
+            {school.name}
+          </h5>
+          <p>{school.address}</p>
+          {school.busAreas.length ? (
+            <p>
+              <span className="font-semibold">送迎対象地域:</span>{" "}
+              {school.busAreas.join("、 ")}
+            </p>
+          ) : null}
+          {school.nearbyStations.length ? (
+            <p>
+              <span className="font-semibold">最寄駅:</span>{" "}
+              {school.nearbyStations.join("、 ")}
+            </p>
+          ) : null}
+        </div>
+      </NavLink>
+    );
+  }
 }
