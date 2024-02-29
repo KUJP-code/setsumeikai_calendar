86361aa3910d9e8b4c55c679e1e7509dcacca8e8
Brett Tanner <bretttanner171@gmail.com>
2024-02-29 17:55:40 +0900

    Sort schools to match .jp list

--- src/components/schools/SchoolList.tsx
+++ src/components/schools/SchoolList.tsx
@@ -22,6 +22,7 @@ export default function SchoolList() {
             school.hiragana.some((hiragana) => hiragana.includes(query)) ||
             school.nearbySchools.some((school) => school.includes(query)),
         );
+  const schoolIdOrder = [31, 11, 7, 5, 4, 3, 13, 16, 8, 6, 22, 26, 9, 15, 10, 24, 12, 23, 25, 36, 30, 17, 21, 33, 34, 27, 14, 32, 28, 20, 18, 29, 19, 35, 2];
 
   return (
     <main className="flex flex-wrap justify-evenly gap-2 px-3 md:px-2">
@@ -30,7 +31,10 @@ export default function SchoolList() {
         className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4"
         ref={parent}
       >
-        {displayedSchools.map((school: school) => {
+        {schoolIdOrder.map((i: number) => {
+		  const school = displayedSchools.find(s => s.id === i.toString());
+		  if (!school) return;
+
           return (
             <SchoolCard
               school={school}
